7566a962413cdc3667d0b733e1506665af275b5f
lizakruger10 <166553686+lizakruger10@users.noreply.github.com>
2024-11-19 22:16:38 -0300

    Create admin.h

---
+++ codigo_completo_projeto3/admin.h
@@ -0,0 +1,15 @@
+#ifndef ADMIN_H
+#define ADMIN_H
+
+void efetuar_login_admin(); // Declara a função de login do administrador
+
+// Declarações das funções de administrador
+void cadastrar_investidor();
+void excluir_investidor();
+void menu_gerente();
+void cadastrar_criptomoedas();
+void excluir_criptomoeda();
+void consultar_extrato();
+void atualizar_cotacao();
+
+#endif
